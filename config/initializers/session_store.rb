61b2809d962b087d4773a1e391ea5c80422407ca
Charles De Groote <charles.degroote@gmail.com>
2014-08-22 22:17:40 +0200

    rename app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_twitter_bitch_es_session'
+Rails.application.config.session_store :cookie_store, key: '_twitter_bitch_bot_session'
