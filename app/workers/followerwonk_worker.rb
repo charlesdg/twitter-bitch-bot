d9087bcc145f2d010f53712ecfb710a4366c4f36
Charles De Groote <charles.degroote@gmail.com>
2014-08-24 11:25:36 +0200

    followerwonk while loop

--- app/workers/followerwonk_worker.rb
+++ app/workers/followerwonk_worker.rb
@@ -4,18 +4,22 @@ class FollowerwonkWorker
   	puts '--- WORKER ---'
   	followers = Follower.select(:username_twitter).where(social_authority: nil).first(25)
 
-  	array = Array.new
-  	followers.each do |f|
-  		array << f.username_twitter
+  	while !followers.empty?
+  		array = Array.new
+	  	followers.each do |f|
+	  		array << f.username_twitter
+	  	end
+
+	    data = Followerwonk.get_social_authority(array)
+
+	    data.each do |d|
+	    	f = Follower.find_by(username_twitter: d[:screen_name])
+	    	f.social_authority = d[:social_authority]
+	    	f.save
+	    end
+	    followers = Follower.select(:username_twitter).where(social_authority: nil).first(25)
   	end
-
-    data = Followerwonk.get_social_authority(array)
-
-    data.each do |d|
-    	f = Follower.find_by(username_twitter: d[:screen_name])
-    	f.social_authority = d[:social_authority]
-    	f.save
-    end
+	  	
 
   end
 end
